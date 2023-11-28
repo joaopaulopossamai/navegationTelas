78ae40576683bb06ae66ab1b8c7b0f1b2689920e
joao paulo possamai <37601838+joaopaulopossamai@users.noreply.github.com>
2023-11-28 19:06:13 -0300

    ,

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
 import * as React from 'react';
 import MyRotes from './src/router';
-import { View, Text } from 'react-native';
-
 
 function App() {
-  return (
-    <MyRotes/>
-  );
+  return <MyRotes />;
 }
 
 export default App;
