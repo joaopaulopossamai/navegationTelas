a382af63e1388ed0f4665c03089d084c950d6fee
joao paulo possamai <37601838+joaopaulopossamai@users.noreply.github.com>
2023-11-28 19:06:51 -0300

    .

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -7,24 +7,17 @@ import TelaFinal from '../screens/TelaFinal';
 import ViewData from '../screens/ViewData';
 import YourData from '../screens/YourDada';
 
-
-
-
 const Stack = createNativeStackNavigator();
-
 
 export default function MyRotes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Unipar">
-        <Stack.Screen options={{headerTransparent:true, headerShown: false}} name="Unipar" component={Unipar} />
-        <Stack.Screen options={{headerTransparent:true, headerShown: false}} name="Home" component={HomeScreen} />
-        <Stack.Screen options={{headerTransparent:true, headerShown: false}} name="Dados" component={YourData} />
-        <Stack.Screen options={{headerTransparent:true, headerShown: false}} name="ViewData" component={ViewData} />
-        <Stack.Screen options={{headerTransparent:true, headerShown: false}} name="Finalizar" component={TelaFinal} />
-
-       
-        
+      <Stack.Navigator initialRouteName='Unipar'>
+        <Stack.Screen options={{ headerTransparent: true, headerShown: false }} name='Unipar' component={Unipar} />
+        <Stack.Screen options={{ headerTransparent: true, headerShown: false }} name='Home' component={HomeScreen} />
+        <Stack.Screen options={{ headerTransparent: true, headerShown: false }} name='Dados' component={YourData} />
+        <Stack.Screen options={{ headerTransparent: true, headerShown: false }} name='ViewData' component={ViewData} />
+        <Stack.Screen options={{ headerTransparent: true, headerShown: false }} name='Finalizar' component={TelaFinal} />
       </Stack.Navigator>
     </NavigationContainer>
   );
